c0d792e6f87757156919dab378a5b6055ea67df4
DruceWillis <dron4ik32@list.ru>
2022-08-22 12:34:31 +0300

    added first task

--- hometask2.cs
+++ hometask2.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// !Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine ("Введите трехзначное число а я найду его вторую цифру");
+int number= Convert.ToInt32(Console.ReadLine());
+if ( number >=100 && number<=999)
+{
+    int ostatok = (number%100)/10;
+    Console.WriteLine("вторая цифра = " + ostatok);
+}
+else
+{
+     Console.WriteLine ("Вы ввели не трехзначное число!!!");
+}
