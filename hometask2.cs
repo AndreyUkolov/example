2e1e294e5f5e8aa50d0d32da98a2cdf8f03c2982
DruceWillis <dron4ik32@list.ru>
2022-08-22 12:54:04 +0300

    added second task

--- hometask2.cs
+++ hometask2.cs
@@ -1,12 +1,27 @@
-﻿// !Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.WriteLine ("Введите трехзначное число а я найду его вторую цифру");
+﻿using System;
+// !Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Console.WriteLine ("Введите трехзначное число а я найду его вторую цифру");
+// int number= Convert.ToInt32(Console.ReadLine());
+// if ( number >=100 && number<=999)
+// {
+//     int ostatok = (number%100)/10;
+//     Console.WriteLine("вторая цифра = " + ostatok);
+// }
+// else
+// {
+//      Console.WriteLine ("Вы ввели не трехзначное число!!!");
+// }
+//______________________________________________________________________________
+//!Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine ("Введите число а я найду его третью цифру");
 int number= Convert.ToInt32(Console.ReadLine());
-if ( number >=100 && number<=999)
+if (number>=100 || number<=-100)
 {
-    int ostatok = (number%100)/10;
-    Console.WriteLine("вторая цифра = " + ostatok);
+    int pochti_otvet = (number % 1000)/100;
+    int otvet = Math.Abs(pochti_otvet);
+    Console.WriteLine("третья цифра = " + otvet);
 }
 else
 {
-     Console.WriteLine ("Вы ввели не трехзначное число!!!");
+    Console.WriteLine ("третьей цифры нет");
 }
